d94f6104319bb5755429f026d58f3a10d6c0679f
Vipul Chaudhary <vpul.chaudhary@gmail.com>
2019-06-01 01:36:59 +0545

    Added dashboard and addmenu to navbar

--- src/Nav.js
+++ src/Nav.js
@@ -12,16 +12,10 @@ const Nav = () => {
             <div className="collapse navbar-collapse" id="navbarNav">
                 <ul className="navbar-nav">
                     <li className="nav-item active">
-                        <a className="nav-link" href="#">Home <span className="sr-only">(current)</span></a>
+                        <a className="nav-link" href="#">Dashboard <span className="sr-only">(current)</span></a>
                     </li>
                     <li className="nav-item">
-                        <a className="nav-link" href="#">Features</a>
-                    </li>
-                    <li className="nav-item">
-                        <a className="nav-link" href="#">Pricing</a>
-                    </li>
-                    <li className="nav-item">
-                        <a className="nav-link disabled" href="#">Disabled</a>
+                        <a className="nav-link" href="#">Add Menu</a>
                     </li>
                 </ul>
             </div>
